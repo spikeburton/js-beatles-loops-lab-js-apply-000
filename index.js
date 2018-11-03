cdbc94e24e376e59943171330eec94e3e7b7c592
Spike Burton <ralphburton3@gmail.com>
2018-11-03 17:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ function johnLennonFacts(facts) {
 
   return facts
 }
+
+function iLoveTheBeatles(n) {
+  var array = []
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while(n <15);
+
+  return array
+}
